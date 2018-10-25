a7b2a20c83ed4aba659b3b4bd12cae2d826f9b39
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2018-10-25 11:08:43 +0530

    tests31

--- NewAWS1/lambda.js
+++ NewAWS1/lambda.js
@@ -1,15 +1,37 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const ddb = new AWS.DynamoDB.DocumentClient();
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
-    ddb.scan({
-        TableName: 'SwaggerDetails'
-    }).promise().then(function (data) {
-        //your logic goes here
-    }).catch(function (err) {
-        //handle error
-    });
+
+    s3.listObjects({
+        'Bucket': 'cf-templates-8j7s4hdepmkt-us-east-1',
+        'MaxKeys': 10,
+        'Prefix': ''
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+             Contents: [
+                {
+                   ETag: "\\"70ee1738b6b21e2c8a43f3a5ab0eee71\\"",
+                   Key: "example1.jpg",
+                   LastModified: <Date Representation>,
+                   Owner: {
+                      DisplayName: "myname",
+                      ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                   },
+                   Size: 11,
+                   StorageClass: "STANDARD"
+                },
+                {...}
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
 
     callback(null, { "message": "Successfully executed" });
